ad964c68ba6e1bd330770e20e40fb7a184c46dfd
grigan-Uragan <prischepny@mail.ru>
2020-07-24 19:50:52 +0300

    5. Tracker - хранилище [#326559]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int count = 0;
+        Item[] result = new Item[size];
+        for (int i = 0; i < result.length; i++) {
+            if (items[i] != null) {
+                result[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count  = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (key != null && key.equals(items[i].getName())) {
+                result[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
