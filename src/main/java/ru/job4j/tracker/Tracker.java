71a23159d1243616d5eb13daabb56eae13a63874
grigan-Uragan <prischepny@mail.ru>
2020-07-24 21:44:01 +0300

    7. Метод удаления заявки Tracker.delete [#326556]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,4 +56,17 @@ public class Tracker {
         }
         return Arrays.copyOf(result, count);
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int start = index + 1;
+            int length = size - index;
+            System.arraycopy(items, start, items, index, length);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
+    }
 }
