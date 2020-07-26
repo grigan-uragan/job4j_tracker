6e0414804b49679cd3c1a47aecd15a71b431555e
grigan-Uragan <prischepny@mail.ru>
2020-07-26 14:36:44 +0300

    5.1. Тестирование. Подготовка данных. [#326488]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -15,6 +16,29 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = {item.getId() + "", "replaced"};
+        StartUI.editItems(new StubInput(answers), tracker);
+        Item item1 = tracker.findById(item.getId());
+        assertThat(item1.getName(), is("replaced"));
+    }
+
+    @Test
+    public void whenDelete() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        int id = item.getId();
+        String[] answers = {id + ""};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item item1 = tracker.findById(id);
+        assertThat(item1, nullValue());
     }
 
 }
