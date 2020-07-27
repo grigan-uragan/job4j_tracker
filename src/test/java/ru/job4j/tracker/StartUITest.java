03199b671c02cfa2435d9bac480ed682de5780d7
grigan-Uragan <prischepny@mail.ru>
2020-07-27 18:42:36 +0300

    9. Написать тесты на StartUI. [#326492]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,5 +8,38 @@ import static org.hamcrest.Matchers.is;
 
 public class StartUITest {
 
+    @Test
+    public void whenCreateItem() {
+        Input input = new StubInput(
+                new String[]{"0", "New Item", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new CreateAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("New Item"));
+    }
 
+    @Test
+    public void whenEditAction() {
+        Input input = new StubInput(
+                new String[]{"0", "1", "Renamed Item", "1"}
+        );
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("New Item"));
+        UserAction[] actions = {new EditAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is("Renamed Item"));
+    }
+
+    @Test
+    public void whenDeleteAction() {
+        Input input = new StubInput(
+                new String[]{"0", "1", "1"}
+        );
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("New Item"));
+        UserAction[] actions = { new DeleteAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId()), nullValue());
+    }
 }
