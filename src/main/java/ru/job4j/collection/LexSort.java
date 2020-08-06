8f6bfbec305bb01537a39c34fe4b8590a39e5b9e
grigan-Uragan <prischepny@mail.ru>
2020-08-06 10:44:21 +0300

    4. Сортировка номера [#326453]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,17 +5,10 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-
         String[] first = o1.split("\\.");
         String[] second = o2.split("\\.");
-        int index  = Math.min(first.length, second.length);
-        for (int i = 0; i < index; i++) {
-           int one = Integer.parseInt(first[i]);
-           int two = Integer.parseInt(second[i]);
-           if (one != two) {
-               return Integer.compare(one, two);
-           }
-        }
-        return 0;
+        int one = Integer.parseInt(first[0]);
+        int two = Integer.parseInt(second[0]);
+        return Integer.compare(one, two);
     }
 }
