4c932640e716dec6003bc9de868b15280b662e02
grigan-Uragan <prischepny@mail.ru>
2020-08-08 22:24:57 +0300

    Отсортировать департаменты [#326448]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,20 +8,9 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String org1, String org2) {
         String[] o1 = org1.split("/");
         String[] o2 = org2.split("/");
-        int result = 0;
-        int len = Math.min(o1.length, o2.length);
-        for (int i = 0; i < len; i++) {
-            if (i != 0) {
-                result = o1[i].compareTo(o2[i]);
-            } else {
-                result = o2[i].compareTo(o1[i]);
-            }
-            if (result != 0) {
-                break;
-            }
-        }
+        int result = o2[0].compareTo(o1[0]);
         if (result == 0) {
-            result = Integer.compare(o1.length, o2.length);
+            result = org1.compareTo(org2);
         }
         return result;
     }
