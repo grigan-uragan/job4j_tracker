f1b06f78b5ccd66b81f156470026d483b8da3796
grigan-Uragan <prischepny@mail.ru>
2020-08-10 21:46:34 +0300

    2.0. Встроенные функциональные интерфейсы. [#326299]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,13 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SearchAtt {
+    public static List<Attachment> filter(List<Attachment> attachments, Predicate<Attachment> predicate) {
+
+        return attachments.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+}
