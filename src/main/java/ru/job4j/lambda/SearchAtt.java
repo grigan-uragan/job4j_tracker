487f2db84c10b9c4a844eaaf941862ffa661a8fd
grigan-Uragan <prischepny@mail.ru>
2020-08-13 02:09:53 +0300

    3. Ссылки на методы [#326294]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -5,7 +5,8 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SearchAtt {
-    public static List<Attachment> filter(List<Attachment> attachments, Predicate<Attachment> predicate) {
+    public static List<Attachment> filter(List<Attachment> attachments,
+                                          Predicate<Attachment> predicate) {
         return attachments.stream().filter(predicate).collect(Collectors.toList());
     }
 }
