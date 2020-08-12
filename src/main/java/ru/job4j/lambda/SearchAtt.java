6bb7699b5a323f9e65496ad2e03b5a28ca46645f
grigan-Uragan <prischepny@mail.ru>
2020-08-12 21:57:40 +0300

    2.0. Встроенные функциональные интерфейсы. [#326299]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -5,8 +5,13 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SearchAtt {
-    public static List<Attachment> filter(List<Attachment> attachments, Predicate<Attachment> predicate) {
-
+    public static List<Attachment> filter(List<Attachment> attachments, String field) {
+        Predicate<Attachment> predicate;
+        if ("name".equals(field)) {
+            predicate = attachment -> attachment.getName().contains("bug");
+        } else {
+            predicate = attachment -> attachment.getSize() > 2;
+        }
         return attachments.stream().filter(predicate).collect(Collectors.toList());
     }
 
---
+++ src/test/java/ru/job4j/lambda/SearchAttTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SearchAttTest {
+    @Test
+    public void whenSearchByName() {
+        Attachment one = new Attachment("one", 1);
+        Attachment two = new Attachment("bugTwo", 2);
+        Attachment three = new Attachment("three", 3);
+        Attachment four = new Attachment("bugFour", 4);
+        Attachment five = new Attachment("five", 5);
+        List<Attachment> attachmentList = Arrays.asList(
+                one, two, three, four, five
+        );
+        List<Attachment> result = SearchAtt.filter(attachmentList, "name");
+        List<Attachment> expected = Arrays.asList(
+                two, four
+        );
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSearchBySize() {
+        Attachment one = new Attachment("one", 1);
+        Attachment two = new Attachment("bugTwo", 2);
+        Attachment three = new Attachment("three", 3);
+        Attachment four = new Attachment("bugFour", 4);
+        Attachment five = new Attachment("five", 5);
+        List<Attachment> attachmentList = Arrays.asList(
+                one, two, three, four, five
+        );
+        List<Attachment> result = SearchAtt.filter(attachmentList, "Noname");
+        List<Attachment> expected = Arrays.asList(
+                three, four, five
+        );
+        assertThat(result, is(expected));
+    }
+
+}
