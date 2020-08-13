d778177ce629befbb6a672792a70ef75f3755a71
grigan-Uragan <prischepny@mail.ru>
2020-08-13 16:47:03 +0300

    3. Уникальность элементов и сортировка. [#326308]

--- src/main/java/stream/TourCompany.java
+++ src/main/java/stream/TourCompany.java
@@ -1,10 +1,15 @@
 package stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TourCompany {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                        .map(Profile::getAddress)
+                        .sorted(Comparator.comparing(Address::getCity))
+                        .distinct()
+                        .collect(Collectors.toList());
     }
 }
