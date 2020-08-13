6ba81447b366f5f4958867990d2429ab5227309b
grigan-Uragan <prischepny@mail.ru>
2020-08-13 17:05:22 +0300

    4. Преобразование List в Map. [#326309]

--- src/main/java/stream/School.java
+++ src/main/java/stream/School.java
@@ -1,13 +1,16 @@
 package stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public static List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+    public static Map<String, Student> collect(List<Student> students,
+                                               Predicate<Student> predicate) {
         return students.stream()
                         .filter(predicate)
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toMap(Student::getSurname,
+                                student -> student));
     }
 }
--- src/test/java/stream/SchoolTest.java
+++ src/test/java/stream/SchoolTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -24,13 +25,14 @@ public class SchoolTest {
                 new Student(90, "Vilkov"),
                 new Student(100, "Vilkova")
         );
-        List<Student> result = School.collect(students,
+        Map<String, Student> result = School.collect(students,
                 student -> student.getScore() >= 70);
-        List<Student> expected = Arrays.asList(
-                new Student(70, "Lisov"),
-                new Student(80, "Lisova"),
-                new Student(90, "Vilkov"),
-                new Student(100, "Vilkova")
+        Map<String, Student> expected = Map.of(
+                "Lisov", new Student(70, "Lisov"),
+                "Lisova", new Student(80, "Lisova"),
+                "Vilkov", new Student(90, "Vilkov"),
+                "Vilkova",  new Student(100, "Vilkova")
+
         );
         assertThat(result, is(expected));
     }
@@ -49,13 +51,12 @@ public class SchoolTest {
                 new Student(90, "Vilkov"),
                 new Student(100, "Vilkova")
         );
-        List<Student> result = School.collect(students,
+        Map<String, Student> result = School.collect(students,
                 student -> student.getScore() < 70
                 && student.getScore() >= 50);
-        List<Student> expected = Arrays.asList(
-                new Student(50, "Sidorov"),
-                new Student(60, "Sidorova")
-
+        Map<String, Student> expected = Map.of(
+                "Sidorov", new Student(50, "Sidorov"),
+                "Sidorova", new Student(60, "Sidorova")
         );
         assertThat(result, is(expected));
     }
@@ -74,14 +75,14 @@ public class SchoolTest {
                 new Student(90, "Vilkov"),
                 new Student(100, "Vilkova")
         );
-        List<Student> result = School.collect(students,
+        Map<String, Student> result = School.collect(students,
                 student -> student.getScore() < 50
                         && student.getScore() > 0);
-        List<Student> expected = Arrays.asList(
-                new Student(10, "Ivanov"),
-                new Student(20, "Ivanova"),
-                new Student(30, "Petrov"),
-                new Student(40, "Petrova")
+        Map<String, Student> expected = Map.of(
+                "Ivanov",  new Student(10, "Ivanov"),
+                "Ivanova", new Student(20, "Ivanova"),
+                "Petrov", new Student(30, "Petrov"),
+                "Petrova", new Student(40, "Petrova")
         );
         assertThat(result, is(expected));
     }
