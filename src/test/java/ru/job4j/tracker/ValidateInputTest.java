ba58ab01566365fa79638101bbc270640cf0ba7c
grigan-Uragan <prischepny@mail.ru>
2021-02-02 18:51:23 +0300

    2.1. Тест на ValidateInput [#326505]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -31,4 +31,30 @@ public class ValidateInputTest {
         ));
     }
 
+    @Test
+    public void whenInvalidInputSevenTimes() {
+        Output output = new StubOutput();
+        Input input = new StubInput(new String[]{
+                "one", "one", "one", "one", "one", "one", "one", "1"});
+        ValidateInput validate = new ValidateInput(input, output);
+        validate.askInt("Enter menu:");
+        assertThat("Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator()
+                + "Please enter validate data again." + System.lineSeparator(),
+                is(output.toString()));
+    }
+
+    @Test
+    public void whenNegativeDigitInput() {
+        Output output = new StubOutput();
+        Input input = new StubInput(new String[]{"-1", "1"});
+        ValidateInput validate = new ValidateInput(input, output);
+        int result = validate.askInt("Enter menu:");
+        assertThat(-1, is(result));
+    }
+
 }
